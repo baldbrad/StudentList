ffb5cbfd07b571075188f35ff105d68683773e23
baldbrad <baldbrad@sheridancollege.ca>
2024-05-23 12:08:33 -0400

    Creation of part-time-student

---
+++ src/studentlist/PartTimeStudent.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author helip
+ */
+public class PartTimeStudent {
+
+    public PartTimeStudent(String name, String studentId, int hours, String address) {
+        this.name = name;
+        this.studentId = studentId;
+        this.hours = hours;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public PartTimeStudent() {
+    }
+    private String name;
+    private String studentId;
+    private int hours;
+    private String address;
+}
